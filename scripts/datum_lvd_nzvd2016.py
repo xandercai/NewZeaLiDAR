74bfd4f6502d4ac32c17376f4e1c057f5c06d48f
Xander Cai <xandcai@gmail.com>
2023-07-27 14:26:46 +1200

    add lyttleton

--- scripts/datum_lvd_nzvd2016.py
+++ scripts/datum_lvd_nzvd2016.py
@@ -15,27 +15,40 @@ from multiprocessing.pool import ThreadPool
 # print('Work dir: ', pathlib.Path.cwd())
 path_list = [
     # not support space in path, need modify directory name from 'Processed Point Cloud' to 'Processed_Point_Cloud'
-    r'../datastorage/LiDAR_Waikato/LiDAR_2014_Hipaua_Thermal_Area/Moturiki1953/Processed_Point_Cloud',
-    r'../datastorage/LiDAR_Waikato/LiDAR_2012_2013_Coromandel/Auckland_1946/Processed_Point_Cloud',
-    r'../datastorage/LiDAR_Waikato/LiDAR_2010_2011/Northern_Waikato/Processed_Point_Cloud/Moturiki_1953',
-    r'../datastorage/LiDAR_Waikato/LiDAR_2010_2011/Raglan_Harbour/Processed_Point_Cloud/Moturiki_1953',
-    r'../datastorage/LiDAR_Waikato/LiDAR_2007_2008/Area_1/XYZI/ground',
-    r'../datastorage/LiDAR_Waikato/LiDAR_2007_2008/Area_1_Option_B/XYZI/ground',
-    r'../datastorage/LiDAR_Waikato/LiDAR_2007_2008/Area_2/XYZI/ground',
-    r'../datastorage/LiDAR_Waikato/LiDAR_2007_2008/Area_3/XYZI/ground',
-    r'../datastorage/LiDAR_Waikato/LiDAR_2007_2008/Area_4/XYZI/ground',
-    r'../datastorage/LiDAR_Waikato/LiDAR_2007_2008/Area_5/XYZI/ground',
-    r'../datastorage/LiDAR_Waikato/LiDAR_2007_2008/Area_6/XYZI/ground',
-    r'../datastorage/LiDAR_Waikato/LiDAR_2006_Lake_Taupo/Moturiki_1953/ground',
+    r'../datastorage/lidar/NZ10_WHope',
+    r'../datastorage/lidar/',
+    r'../datastorage/lidar_waikato/LiDAR_2014_Hipaua_Thermal_Area/Moturiki1953/Processed_Point_Cloud',
+    r'../datastorage/lidar_waikato/LiDAR_2012_2013_Coromandel/Auckland_1946/Processed_Point_Cloud',
+    r'../datastorage/lidar_waikato/LiDAR_2010_2011/Northern_Waikato/Processed_Point_Cloud/Moturiki_1953',
+    r'../datastorage/lidar_waikato/LiDAR_2010_2011/Raglan_Harbour/Processed_Point_Cloud/Moturiki_1953',
+    r'../datastorage/lidar_waikato/LiDAR_2007_2008/Area_1/XYZI/ground',
+    r'../datastorage/lidar_waikato/LiDAR_2007_2008/Area_1_Option_B/XYZI/ground',
+    r'../datastorage/lidar_waikato/LiDAR_2007_2008/Area_2/XYZI/ground',
+    r'../datastorage/lidar_waikato/LiDAR_2007_2008/Area_3/XYZI/ground',
+    r'../datastorage/lidar_waikato/LiDAR_2007_2008/Area_4/XYZI/ground',
+    r'../datastorage/lidar_waikato/LiDAR_2007_2008/Area_5/XYZI/ground',
+    r'../datastorage/lidar_waikato/LiDAR_2007_2008/Area_6/XYZI/ground',
+    r'../datastorage/lidar_waikato/LiDAR_2006_Lake_Taupo/Moturiki_1953/ground',
 ]
 assert int(sys.argv[1]) < len(path_list), 'Input Index out of range!'
 src_dir = pathlib.Path(path_list[int(sys.argv[1])])
 print('Transforming datum from LVD to NZVD2016 in dir:\n', src_dir)
 
+# for Lyttleton_1937: NZ10_WHope NZ10_CAlpine
+if 'NZ10_WHope' in str(src_dir) or 'NZ10_CAlpine' in str(src_dir):
+    # change .laz file suffix to .las
+    laz_files = [f for f in src_dir.glob('*.laz')]
+    print(f'Changing {len(laz_files)} .laz files to .las...')
+    for f in laz_files:
+        f.rename(src_dir / f.stem / '.las')
+
 # gtx file source: https://github.com/linz/proj-datumgrid-nz
 gtxfile_Moturiki_1953 = r'../proj-datumgrid-nz/files/moturiki_1953.gtx'
 gtxfile_Auckland_1946 = r'../proj-datumgrid-nz/files/auckht1946-nzvd2016.gtx'
-if 'Auckland_1946' in str(src_dir):
+gtxfile_Lyttleton_1937 = r'../proj-datumgrid-nz/files/lyttht1937-nzvd2016.gtx'
+if 'NZ10_WHope' in str(src_dir) or 'NZ10_CAlpine' in str(src_dir):
+    gtxfile = gtxfile_Lyttleton_1937
+elif 'Auckland_1946' in str(src_dir):
     gtxfile = gtxfile_Auckland_1946
 else:
     gtxfile = gtxfile_Moturiki_1953
@@ -45,9 +58,11 @@ pipeline_las = r'configs/pipeline_las.json'
 pipeline_xyz = r'configs/pipeline_xyz.json'
 horizontal_srs = 'EPSG:2193'
 
-dest_dir = src_dir.parent.parent / pathlib.Path('NZVD2016')
-if not os.path.exists(dest_dir):
-    os.mkdir(dest_dir)
+if 'NZ10_WHope' in str(src_dir) or 'NZ10_CAlpine' in str(src_dir):
+    dest_dir = src_dir / pathlib.Path('NZVD2016')
+else:  # waikato_lidar
+    dest_dir = src_dir.parent.parent / pathlib.Path('NZVD2016')
+dest_dir.mkdir(parents=True, exist_ok=True)
 
 pdal_cmd_list = []
 
